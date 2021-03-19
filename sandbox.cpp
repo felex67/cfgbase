f92625236b35638777c122c99acf9a8f384536ee
felexxx67 <felexxx67@gmail.com>
2021-03-19 04:56:54 +0200

    qq

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,27 +1,17 @@
 #include <cstdio>
 #include <iostream>
 
-#include "config.hpp"
-
-struct _LogConfig {
-    cfg_inst_t Conf = {"log.conf", "config", sizeof(*this) };
-    struct _Mode {
-        cfg_group_t __group = { "Mode", sizeof(*this) };
-        cfg_string_t RunAs = { "RunAs", "thread" };
-    } Mode;
-    struct _Path {
-        cfg_group_t __group = { "Path", sizeof(*this) };
-        cfg_string_t Pipe = { "Pipe", "/var/tmp/flxwd-logpipe.pipe" };
-    } Path;
-    struct _Format {
-        cfg_group_t __group = { "Format", sizeof(*this) };
-        cfg_string_t Datetime = { "Datetime", "%X %x" };
-    } Format;
-} LogConfig;
-
 int main(const int argc, const char* argv[]) {
-    LogConfig.Conf.load();
-    std::cout << LogConfig.Conf.toString() << std::endl;
-    LogConfig.Conf.save();
+    std::string name = "hello";
+    const char *var = "hello=world\n";
+    const char *N = name.c_str();
+    size_t n = name.length();
+    for (size_t i = 0; i < n; i++) {
+        if (N[i] == var[i]);
+        else {
+            std::cout << "Not same" << std::endl;
+        }
+    }
+    std::cout << "Same" << std::endl;
     return 0;
 }
